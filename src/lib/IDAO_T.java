9d3a0db22f1a8332386669999cfe0170bc70d9d6
Kelvin De Costa <kelvin.costa@universo.univates.br>
2020-10-15 21:59:47 -0300

    Pacote de apoio

---
+++ src/lib/IDAO_T.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pretto
+ */
+
+// Utiliza Generics como tipo de dado
+
+public interface IDAO_T <T> {
+
+    public boolean salvar(T o);
+
+    public boolean atualizar(T o);
+
+    public boolean excluir(int id);
+
+    public ArrayList<T> consultarTodos();
+
+    public ArrayList<T> consultar(String criterio);
+
+    public T consultarId(int id);
+}
