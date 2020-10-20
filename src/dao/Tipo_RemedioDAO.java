b182a52499158b940be8f02f5c08e8d4f44a7a0b
Kelvin De Costa <kelvin.costa@universo.univates.br>
2020-10-20 21:05:32 -0200

    DAO Tipo_Remédio

---
+++ src/dao/Tipo_RemedioDAO.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import control.Tipo_Remedio;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import lib.ConexaoBD;
+import lib.IDAO_T;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Tipo_RemedioDAO implements IDAO_T<Tipo_Remedio> {
+
+    private ResultSet resultadoQ = null;
+
+    @Override
+    public boolean salvar(Tipo_Remedio o) {
+        try {
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+            String sql = "";
+            if (o.getId_tpremedio() == 0) {
+                sql = "INSERT INTO tipo_remedio VALUES ("
+                        + "DEFAULT, "
+                        + "'" + o.getDescricao() + "',"
+                        + "'" + o.getSituacao() + "')";
+            } else {
+                sql = "UPDATE tipo_remedio "
+                        + "SET descricao = '" + o.getDescricao() + "',"
+                        + "situacao = '" + o.getSituacao() + "',"
+                        + "WHERE id = " + o.getId_tpremedio();
+            }
+
+            System.out.println("SQL: " + sql);
+
+            int resultado = st.executeUpdate(sql);
+
+            return true;
+        } catch (Exception e) {
+            System.out.println("Erro ao salvar Tipo de Remédio = " + e);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean atualizar(Tipo_Remedio o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean excluir(int id) {
+        try {
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            String sql = "DELETE "
+                    + "FROM tipo_remedio "
+                    + "WHERE id = " + id;
+
+            System.out.println("SQL: " + sql);
+
+            int resultado = st.executeUpdate(sql);
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Erro ao excluir: " + e);
+            return false;
+        }
+    }
+
+    @Override
+    public ArrayList<Tipo_Remedio> consultarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Tipo_Remedio> consultar(String criterio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Tipo_Remedio consultarId(int id) {
+        Tipo_Remedio tpRemedio = null;
+
+        try {
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            String sql = "SELECT * "
+                    + "FROM tipo_remedio "
+                    + "WHERE id = " + id;
+
+            System.out.println("SQL: " + sql);
+
+            // executa consulta
+            resultadoQ = st.executeQuery(sql);
+
+            // avanca ResultSet
+            if (resultadoQ.next()) {
+                tpRemedio = new Tipo_Remedio();
+
+                // obtem dados do RS
+                tpRemedio.setId_tpremedio(resultadoQ.getInt("id_tpremedio"));
+                tpRemedio.setDescricao(resultadoQ.getString("descricao"));
+                tpRemedio.setSituacao(resultadoQ.getString("situacao"));
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro ao consultar: " + e);
+        }
+        return tpRemedio;
+    }
+}
