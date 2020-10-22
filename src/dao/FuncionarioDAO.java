7510ee1a2e51a5426c844f8d505c0020d1ea5cf1
Kelvin De Costa <kelvin.costa@universo.univates.br>
2020-10-21 23:30:38 -0200

    Popular Tabela FuncionarioDAO

--- src/dao/FuncionarioDAO.java
+++ src/dao/FuncionarioDAO.java
@@ -6,9 +6,15 @@
 package dao;
 
 import control.Funcionario;
+import java.awt.Color;
+import java.awt.Component;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import lib.ConexaoBD;
 import lib.IDAO_T;
 
@@ -129,9 +135,9 @@ public class FuncionarioDAO implements IDAO_T<Funcionario> {
                 pessoa.setNome(resultadoQ.getString("nome"));
                 pessoa.setCpf(resultadoQ.getString("cpf"));
                 pessoa.setRg(resultadoQ.getString("rg"));
-                pessoa.setDt_nasc(resultadoQ.getTime("dt_nasci"));  //verificar se o getTime ta certo
-                pessoa.setDt_admissao(resultadoQ.getTime("dt_admissao"));  //verificar se o getTime ta certo
-                pessoa.setDt_demissao(resultadoQ.getTime("dt_demissao"));  //verificar se o getTime ta certo
+                pessoa.setDt_nasc(resultadoQ.getDate("dt_nasci"));  
+                pessoa.setDt_admissao(resultadoQ.getDate("dt_admissao"));  
+                pessoa.setDt_demissao(resultadoQ.getDate("dt_demissao"));  
                 pessoa.setSexo(resultadoQ.getString("sexo"));
                 pessoa.setTelefone(resultadoQ.getString("telefone"));
                 pessoa.setId_contrato(resultadoQ.getInt("id_contrato"));
@@ -145,4 +151,131 @@ public class FuncionarioDAO implements IDAO_T<Funcionario> {
         }
         return pessoa;
     }
+    
+    public void popularTabela(JTable tabela, String criterio) {
+        // dados da tabela
+        Object[][] dadosTabela = null;
+
+        // cabecalho da tabela
+        Object[] cabecalho = new Object[7];
+        cabecalho[0] = "Código";
+        cabecalho[1] = "Nome";
+        cabecalho[2] = "Cpf";
+        cabecalho[3] = "Rg";
+        cabecalho[4] = "Data de admissão";
+        cabecalho[5] = "Sexo";
+        cabecalho[6] = "Telefone";
+
+        // cria matriz de acordo com nº de registros da tabela
+        try {
+            resultadoQ = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(""
+                    + "SELECT count(*) "
+                    + "FROM funcionario "
+                    + "WHERE "
+                    + "NOME ILIKE '%" + criterio + "%'");
+
+            resultadoQ.next();
+
+            dadosTabela = new Object[resultadoQ.getInt(1)][7];
+
+        } catch (Exception e) {
+            System.out.println("Erro ao consultar XXX: " + e);
+        }
+
+        int lin = 0;
+
+        // efetua consulta na tabela
+        try {
+            resultadoQ = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(""
+                    + "SELECT * "
+                    + "FROM funcionario "
+                    + "WHERE "
+                    + "NOME ILIKE '%" + criterio + "%' ORDER BY id_func");
+
+            while (resultadoQ.next()) {
+
+                dadosTabela[lin][0] = resultadoQ.getInt("id_func");
+                dadosTabela[lin][1] = resultadoQ.getString("nome");
+                dadosTabela[lin][2] = resultadoQ.getString("cpf");
+                dadosTabela[lin][3] = resultadoQ.getString("rg");
+                dadosTabela[lin][4] = resultadoQ.getDate("data_admissao");
+                dadosTabela[lin][5] = resultadoQ.getString("sexo");
+                dadosTabela[lin][6] = resultadoQ.getString("telefone");
+
+                // caso a coluna precise exibir uma imagem
+//                if (resultadoQ.getBoolean("Situacao")) {
+//                    dadosTabela[lin][2] = new ImageIcon(getClass().getClassLoader().getResource("Interface/imagens/status_ativo.png"));
+//                } else {
+//                    dadosTabela[lin][2] = new ImageIcon(getClass().getClassLoader().getResource("Interface/imagens/status_inativo.png"));
+//                }
+                lin++;
+            }
+        } catch (Exception e) {
+            System.out.println("problemas para popular tabela...");
+            System.out.println(e);
+        }
+
+        // configuracoes adicionais no componente tabela
+        tabela.setModel(new DefaultTableModel(dadosTabela, cabecalho) {
+            @Override
+            // quando retorno for FALSE, a tabela nao é editavel
+            public boolean isCellEditable(int row, int column) {
+                return false;
+                /*  
+                 if (column == 3) {  // apenas a coluna 3 sera editavel
+                 return true;
+                 } else {
+                 return false;
+                 }
+                 */
+            }
+
+            // alteracao no metodo que determina a coluna em que o objeto ImageIcon devera aparecer
+            @Override
+            public Class getColumnClass(int column) {
+
+                if (column == 7) {
+//                    return ImageIcon.class;
+                }
+                return Object.class;
+            }
+        });
+
+        // permite seleção de apenas uma linha da tabela
+        tabela.setSelectionMode(0);
+
+        // redimensiona as colunas de uma tabela
+        TableColumn column = null;
+        for (int i = 0; i < tabela.getColumnCount(); i++) {
+            column = tabela.getColumnModel().getColumn(i);
+            switch (i) {
+                case 0:
+                    column.setPreferredWidth(17);
+                    break;
+                case 1:
+                    column.setPreferredWidth(140);
+                    break;
+//                case 2:
+//                    column.setPreferredWidth(14);
+//                    break;
+            }
+        }
+        // renderizacao das linhas da tabela = mudar a cor
+        tabela.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value,
+                    boolean isSelected, boolean hasFocus, int row, int column) {
+                super.getTableCellRendererComponent(table, value, isSelected,
+                        hasFocus, row, column);
+                if (row % 2 == 0) {
+                    setBackground(Color.GRAY);
+                } else {
+                    setBackground(Color.LIGHT_GRAY);
+                }
+                return this;
+            }
+        });
+    }
+       
 }
