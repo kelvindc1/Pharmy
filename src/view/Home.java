31462ae0c1018254939b626b156113b2b6b2266b
Kelvin De Costa <kelvin.costa@universo.univates.br>
2020-10-27 20:00:18 -0200

    Ajustes

--- src/view/Home.java
+++ src/view/Home.java
@@ -17,8 +17,8 @@ public class Home extends javax.swing.JFrame {
      * Creates new form Home
      */
     public Home() {
-//        administrationTabs.hide();
-   
+//     administrationTabs.hide();
+
         initComponents();
     }
 
